af5cd963ac73cc9b27582f5fb2b31f9141a2a097
timmPrice <timmPrice6@gmail.com>
2025-08-19 00:47:56 -0400

    import tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './style.css'
+
+import Navbar from '.componets/navbar'
+import Home from '.componets/home'
+import Work from '.componets/work'
+import Post from '.componets/post'
+import Uses from '.componets/uses'
+import Skills from '.componets/skills'
+import Links from '.componets/links'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <main className = "text-gray-400 bg-gray-900 body-font">
+            <Navbar />
+            <Home />
+            <Work />
+            <Post />
+            <Uses />
+            <Skills />
+            <Links />
+        </main>
+    );
 }
 
 export default App
