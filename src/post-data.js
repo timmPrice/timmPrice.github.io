6f4c6727f5b2aa5ba45089576e1810840de1afe0
timmPrice <timmPrice6@gmail.com>
2026-02-08 05:57:14 -0500

    working on post generation

---
+++ src/post-data.js
@@ -0,0 +1,12 @@
+var posts = [
+    {
+        title: "Integration by Parts",
+        date: "",
+        filename: "Integration by Parts.html",
+    },
+    {
+        title: "Trigonometric Substitution",
+        date: "",
+        filename: "Trigonometric Substitution.html",
+    },
+]
